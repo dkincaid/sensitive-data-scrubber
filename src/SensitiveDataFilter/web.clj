6ec96e37e5f00d4d8efbcd2d44c352516b75360f
Dave Kincaid <kincaid.dave@gmail.com>
2012-08-01 08:59:49 -0500

    Changes

--- src/SensitiveDataFilter/web.clj
+++ src/SensitiveDataFilter/web.clj
@@ -4,7 +4,13 @@
             [noir.response :as response])
   (:use [noir.core]
         [hiccup.core]
-        [hiccup.form]))
+        [hiccup.form]
+        [hiccup.element]))
+
+(def test-cards ["378282246310005" "371449635398431" "378734493671000" "5610591081018250"
+                 "30569309025904" "38520000023237" "6011111111111117" "6011000990139424"
+                 "3530111333300000" "3566002020360505" "5555555555554444" "5105105105105100"
+                 "4111111111111111" "4012888888881881" "4222222222222"])
 
 (defpartial layout [& content]
   (html
@@ -22,6 +28,13 @@
   (label "scrubbed-text" "Scrubbed text: ")
   (text-field {:width 40}  "scrubbed-text" scrubbed-value))
 
+(defpartial sample-cards []
+  (html
+   [:p "Test credit card numbers:"]
+   [:ul
+    (map #(vector :li %) test-cards)]
+   (link-to "https://www.paypalobjects.com/en_US/vhelp/paypalmanager_help/credit_card_numbers.htm" "Source")))
+
 (defpage "/" {:as scrubbed-text}
   (layout
    (form-to [:post "/"]
@@ -29,6 +42,7 @@
             (submit-button "Scrub")
             (html [:p])
             (scrubbed-data scrubbed-text)
+            (sample-cards)
             )))
 
 (defpage [:post "/"] {:keys [text]}
